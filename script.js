3b65ed588754ce6833e01a279b0910b354193501
aduong3 <mythalink@gmail.com>
2025-01-20 20:31:48 -0800

    add basic css resets and start script.js to add into library and display info on DOM
    need to add content for a basic "add book" form and display it on the page.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Test");
+const myLibrary = [];
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary(title, author, pages) {
+  myLibrary.push(new Book(title, author, pages));
+}
+
+function LoopThroughBooks() {
+  const table = document.querySelector("table");
+
+  myLibrary.forEach((book) => {
+    console.log(book.title, book.author);
+    // let html = `<tr>
+    // <td>${book.title}</td>
+    // <td>${book.author}</td>
+    // </tr>`;
+    // table.innerHTML += html;
+  });
+}
+
+addBookToLibrary("test", "test-author");
+LoopThroughBooks();
