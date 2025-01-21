6d939177a4d889cf1290994aabb30383f0c92f30
aduong3 <mythalink@gmail.com>
2025-01-20 21:57:40 -0800

    set up form with javascript and work on the styling for book display
    The top part is done with styling, maybe some tweaks to enhance it further. The book display will have the side highlight on each card, but it will be coded so JavaScript will tell it to be either green or red depending on if they read it.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, hasRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.hasRead = hasRead;
 }
 
-function addBookToLibrary(title, author, pages) {
-  myLibrary.push(new Book(title, author, pages));
+function addBookToLibrary(title, author, pages, hasRead) {
+  myLibrary.push(new Book(title, author, pages, hasRead));
 }
 
 function LoopThroughBooks() {
